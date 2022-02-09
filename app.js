d94a5210caf0525ea0045425a5e0bb8ac09de938
Michela-DC <micheladelconte@outlook.com>
2022-02-09 18:20:23 +0100

    Added dice game

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* 
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
@@ -10,10 +11,10 @@ console.log(authorized);
 const userEmail = prompt('Ciao! Inserisci qui la tua email.');
 console.log(userEmail);
 
-let login = false;
-
 // Creo ciclo for che passa dentro l'array partendo da 0 e arriva alla fine dell'array (per arrivare a fine array uso .length)
     // --> all'interno del for creo un if per vedere se l'email dell'utente è tra quelle autorizzate - uso un valore booleano per tenerne conto
+
+let login = false;
 
 for(let i = 0; i < authorized.length; i++){
     console.log(authorized[i]);
@@ -32,10 +33,29 @@ if (login === true){
     alert ("ACCESSO NEGATO! Spiacente non sei autorizzato!")
 }
 
+*/
 
 
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi ha il numero più alto.  (e se pari?)
 
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer usando Math.floor e Math.randon
 
+const userNumber = Math.floor( Math.random()* 6 + 1 );
+console.log(userNumber);
 
-// Creo ciclo for che passa dentro l'array 
-//     --> all'interno del for creo uf if per vedere se l'email dell'utente è tra quelle autorizzate
+const computerNumber = Math.floor( Math.random()* 6 + 1 );
+console.log(computerNumber);
+
+// Creo degli if per verificare chi vince o se sono pari
+
+if( userNumber > computerNumber ) {
+    console.log("Ha vinto l'utente");
+
+} else if (computerNumber > userNumber) {
+    console.log("Ha vinto il comupter");
+
+} else {
+    console.log("PARI!!!");
+}
